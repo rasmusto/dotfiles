adf2def74894247267ef2994486dedb5f120f449
Torben Rasmussen <rtorben@gmail.com>
2011-05-06 20:01:34 -0700

    now with more symlinks!

--- setup.sh
+++ setup.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-#Download the tarball of config files
-wget http://dl.dropbox.com/u/1857337/setup/files.tar.gz
+echo 'Creating symlinks...'
 
-if [ -e files.tar.gz ];
-then
-    echo 'Extracting files...'
-else
-    exit
-fi
-tar -xf files.tar.gz
-echo 'Moving files into place...'
-mv ./files/_bashrc ~/.bashrc
-mv ./files/_vimrc ~/.vimrc
-mv ./files/_gvimrc ~/.gvimrc
-mv ./files/_zshrc ~/.zshrc
-mv ./files/_zshrc.local ~/.zshrc.local
-mv ./files/_zshrc.pre ~/.zshrc.pre
-mv ./files/_gitconfig ~/.gitconfig
-mv ./files/subversion ~/.subversion
-mv ./files/vimfiles ~/.vim
-echo 'Cleaning up...'
-rm -rf ./files
-rm files.tar.gz
+ln -s ./files/_bashrc ~/.bashrc
+ln -s ./files/_vimrc ~/.vimrc
+ln -s ./files/_gvimrc ~/.gvimrc
+ln -s ./files/_zshrc ~/.zshrc
+ln -s ./files/_zshrc.local ~/.zshrc.local
+ln -s ./files/_zshrc.pre ~/.zshrc.pre
+ln -s ./files/_gitconfig ~/.gitconfig
+ln -s ./files/subversion ~/.subversion
+ln -s ./files/vimfiles ~/.vim
+
 echo 'Done!'
