8402fc2cb876e3b16db639178ba604096cb354fa
torben <torben@latpop.(none)>
2011-05-11 10:29:48 -0700

    changed the symlink paths

--- setup.sh
+++ setup.sh
@@ -3,25 +3,25 @@
 echo 'Creating symlinks...'
 
 #bash
-ln -s ./files/_bashrc ~/.bashrc
+ln -s ~/setup/files/_bashrc ~/.bashrc
 
 #vim
-ln -s ./files/_vimrc ~/.vimrc
-ln -s ./files/_gvimrc ~/.gvimrc
-ln -s ./files/vimfiles ~/.vim
+ln -s ~/setup/files/_vimrc ~/.vimrc
+ln -s ~/setup/files/_gvimrc ~/.gvimrc
+ln -s ~/setup/files/vimfiles ~/.vim
 
 #zsh
-ln -s ./files/_zshrc ~/.zshrc
-ln -s ./files/_zshrc.local ~/.zshrc.local
-ln -s ./files/_zshrc.pre ~/.zshrc.pre
+ln -s ~/setup/files/_zshrc ~/.zshrc
+ln -s ~/setup/files/_zshrc.local ~/zshrc.local
+ln -s ~/setup/files/_zshrc.pre ~/.zshrc.pre
 
 #git
-ln -s ./files/_gitconfig ~/.gitconfig
+ln -s ~/setup/files/_gitconfig ~/.gitconfig
 
 #svn
-ln -s ./files/subversion ~/.subversion
+ln -s ~/setup/files/subversion ~/.subversion
 
 #scripts
-ln -s ./files/bin ~/.bin
+ln -s ~/setup/files/bin ~/.bin
 
 echo 'Done!'
