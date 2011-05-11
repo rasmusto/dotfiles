80ffe5e3433c6aa7c72c172a59df4e010abbf812
Torben Rasmussen <rtorben@gmail.com>
2011-05-11 10:36:13 -0700

    directories don't break now

--- setup.sh
+++ setup.sh
@@ -8,20 +8,20 @@ ln -s ~/setup/files/_bashrc ~/.bashrc
 #vim
 ln -s ~/setup/files/_vimrc ~/.vimrc
 ln -s ~/setup/files/_gvimrc ~/.gvimrc
-ln -s ~/setup/files/vimfiles ~/.vim
+ln -s -n ~/setup/files/vimfiles ~/.vim
 
 #zsh
 ln -s ~/setup/files/_zshrc ~/.zshrc
-ln -s ~/setup/files/_zshrc.local ~/zshrc.local
+ln -s ~/setup/files/_zshrc.local ~/.zshrc.local
 ln -s ~/setup/files/_zshrc.pre ~/.zshrc.pre
 
 #git
 ln -s ~/setup/files/_gitconfig ~/.gitconfig
 
 #svn
-ln -s ~/setup/files/subversion ~/.subversion
+ln -s -n ~/setup/files/subversion ~/.subversion
 
 #scripts
-ln -s ~/setup/files/bin ~/.bin
+ln -s -n ~/setup/files/bin ~/.bin
 
 echo 'Done!'
