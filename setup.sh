888b367177eb4a400d1fca6e7b3b922445da602f
Torben Rasmussen <trasmuss@lvd074.(none)>
2011-07-29 11:41:29 -0700

    more changes

--- setup.sh
+++ setup.sh
@@ -4,38 +4,42 @@ os=`uname`
 #windows setup
 if [[ "$os" =~ ".*NT.*" ]]
 then
-	echo "Run setup.bat to set up windows"
-	echo
+    echo "Run setup.bat to set up windows"
+    echo
 
 fi
 
 if [[ "$os" =~ "Linux" ]]
 then
-	echo "it's linux!"
-	#bash
-	if [ -f ~/.bashrc ]; then mv ~/.bashrc ~/.bashrc.bak; fi
-	ln -s ./_bashrc ~/.bashrc
+    echo "it's linux!"
+    #bash
+    if [ -f ~/.bashrc ]; then mv ~/.bashrc ~/.bashrc.bak; fi
+    ln -s ./_bashrc ~/.bashrc
 
-	#vim
-	if [ -f ~/.vimrc ]; then mv ~/.vimrc ~/.vimrc.bak; fi
-	ln -s ./files/_vimrc ~/.vimrc 
-	if [ -f ~/.gvimrc ]; then mv ~/.gvimrc ~/.gvimrc.bak; fi 
-	ln -s ./files/_gvimrc ~/.gvimrc
+    #vim
+    if [ -f ~/.vimrc ]; then mv ~/.vimrc ~/.vimrc.bak; fi
+    ln -s ./files/_vimrc ~/.vimrc 
+    if [ -f ~/.gvimrc ]; then mv ~/.gvimrc ~/.gvimrc.bak; fi 
+    ln -s ./files/_gvimrc ~/.gvimrc
 
-	if [ -d ~/.vim ]; then mv ~/.vim ~/.vim.bak; fi
-	ln -s -n ./files/vimfiles ~/.vim
+    if [ -d ~/.vim ]; then mv ~/.vim ~/.vim.bak; fi
+    ln -s -n ./files/vimfiles ~/.vim
 
-	#zsh
-	if [ -f ~/.zshrc ]; then mv ~/.zshrc ~/.zshrc.bak; fi
-	ln -s ./files/_zshrc ~/.zshrc
+    #zsh
+    if [ -f ~/.zshrc ]; then mv ~/.zshrc ~/.zshrc.bak; fi
+    ln -s ./files/_zshrc ~/.zshrc
 
-	#svn
-	if [ -f ~/.subversion ]; then mv ~/.subversion ~/.subversion.bak; fi
-	ln -s -n ./files/subversion ~/.subversion
+    #svn
+    if [ -d ~/.subversion ]; then mv ~/.subversion ~/.subversion.bak; fi
+    ln -s -n ./files/subversion ~/.subversion
 
-	#scripts
-	if [ -d ~/.bin ]; then mv ~/.bin ~/.bin.bak; fi
-	ln -s -n ./files/bin ~/.bin
+    #scripts
+    if [ -d ~/.bin ]; then mv ~/.bin ~/.bin.bak; fi
+    ln -s -n ./files/bin ~/.bin
+
+    #tmux
+    if [ -f ~/.tmux.conf ]; then mv ~/.tmux.conf ~/.tmux.conf.bak; fi
+    ln -s -n ./files/_tmux.conf ~/.tmux.conf
 fi
 
 exit
